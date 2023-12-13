7c1108725b3e76ef170db86dd651c2ae604f9717
vetukuri <varmavetukuri1234@gmail.com>
2023-12-13 17:13:45 +0530

    check

--- Test_sdk/build.gradle.kts
+++ Test_sdk/build.gradle.kts
@@ -44,6 +44,7 @@ dependencies {
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
+    implementation("com.github.Tejovarmabuddyloan:Test_sdk:-SNAPSHOT")
 }
 
 publishing {
@@ -53,7 +54,6 @@ publishing {
             artifactId = "Test_sdk"
             version = "1.0.0"
 
-            from(components["release"])
         }
     }
 }
